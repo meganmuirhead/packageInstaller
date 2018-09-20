e4e253d2053685ecb374c817c60c16b6e642eda7
meganmuirhead <megan@s56.net>
2018-09-19 22:37:08 -0600

    working on test

---
+++ src/DependencyUtils.js
@@ -0,0 +1,17 @@
+
+describe("DependencyUtils", () => {
+
+    describe('first dependency added', () => {
+        it('kitten service and ice should be able to be added without anything installed', () => {
+            let inputToBeTested = [
+                'KittenService: ',
+                'Leetmeme: Cyberportal',
+                'Cyberportal: Ice',
+                'CamelCaser: KittenService',
+                'Fraudstream: Leetmeme',
+                'Ice: '
+            ];
+
+    })
+
+});
